4cd42a5cd86a45100d8cdddae2bddd608d61c275
BalqeesA <basbosared@ymail.com>
2021-02-09 01:30:57 +0200

    upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+
+    var favDrink = prompt("What's your favorite cocktail drink?");
+      var text; 
+           switch(favDrink) {
+              case "juice":
+                text = "Excellent choice! Martini is good for your soul.";
+                break;
+              case "water":
+                text = "Daiquiri is my favorite too!";
+                break;
+                default:
+                text = "nice choice";
+                break;
+            }
+             
+             console.log(favDrink);
